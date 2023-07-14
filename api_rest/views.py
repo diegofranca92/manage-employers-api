73d2958248c5d5bf560fad26da10343e32c82e83
Diego França <diego.franca92@gmail.com>
2023-07-13 21:39:17 -0300

    feat: create user serializer, views and urls

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+@api_view(['GET'])
+def get_users(request):
+  if request.method == 'GET':
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
+  
+  return Response(status=status.HTTP_400_BAD_REQUEST)
