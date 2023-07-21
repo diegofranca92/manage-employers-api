6740c84eefff0c8d05075be099083a6d476293d3
Diego França <diego.franca92@gmail.com>
2023-07-21 20:35:14 -0300

    feat: model and serializer begin

---
+++ core/models/user.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# https://dev.to/ki3ani/implementing-jwt-authentication-and-user-profile-with-django-rest-api-part-3-3dh9
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255, blank=True)
